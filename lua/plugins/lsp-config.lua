693236773581ba3473f70f0d84604f20ddb009c3
Weston Dransfield <wdransfield@instructure.com>
2025-10-07 22:09:57 -0600

    lsp bug fixes

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,7 +2,10 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      -- Disable solargraph completely - only use ruby_lsp
       servers = {
+        solargraph = false, -- Explicitly disable
+        
         ruby_lsp = {
           -- Custom root_dir function to always use outermost git repo
           root_dir = function(fname)
@@ -61,47 +64,6 @@ return {
             },
           },
           settings = {},
-        },
-
-        -- Configure solargraph the same way (in case you switch)
-        solargraph = {
-          root_dir = function(fname)
-            local util = require("lspconfig.util")
-
-            local function find_outermost_git_root(path)
-              if not path or path == "" then
-                return nil
-              end
-              
-              local current_path = path
-              local git_root = nil
-              local max_iter = 50
-              local iter = 0
-
-              while current_path ~= "/" and iter < max_iter do
-                if vim.fn.isdirectory(current_path .. "/.git") == 1 then
-                  git_root = current_path
-                end
-                local parent = vim.fn.fnamemodify(current_path, ":h")
-                if parent == current_path then
-                  break
-                end
-                current_path = parent
-                iter = iter + 1
-              end
-
-              return git_root
-            end
-
-            local start_path = vim.fn.fnamemodify(fname, ":h")
-            local outermost_root = find_outermost_git_root(start_path)
-
-            if outermost_root then
-              return outermost_root
-            end
-
-            return util.root_pattern("Gemfile", ".git")(fname)
-          end,
         },
       },
     },
